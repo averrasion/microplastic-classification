4c68eaee01d8896231c33e93429f8e9469513893
vgoliga <197795812+vgoliga@users.noreply.github.com>
2025-06-30 14:16:15 +0200

    Added full data set write
    Cleaned names. Included unique id for each observation.

--- scripts/mutations.R
+++ scripts/mutations.R
@@ -6,12 +6,20 @@ cols <- cols_only(
   L_BlackBackground = col_double(), a_BlackBackground = col_double(),
   b_BlackBackground = col_double(), L_GreenBackground = col_double(),
   a_GreenBackground = col_double(), b_GreenBackground = col_double(),
-  Color_sd = col_double()
+  Color_sd = col_double(), Group = col_character(), RF_use = col_logical(),
+  File_Name = col_character(), Particle_Num = col_integer()
 )
 
-paths <- list.files("data", full.names = T)
-paths <- paths |> set_names(basename)
-files <- map(paths, \(path) read_csv(path, col_types = cols))
-data <- list_rbind(files, names_to = "file")
-data <- data |> janitor::clean_names()
-data <- data |> separate_wider_delim(file, "_", names = c("project", NA))
+data <- list.files("data", pattern = "*.csv$", full.names = T) |> 
+  map(\(path) read_csv(path, col_types = cols)) |> 
+  list_rbind() |>  janitor::clean_names()
+
+data <- data |> distinct(file_name, particle_num, .keep_all = TRUE)
+
+data <- data |> mutate(
+  id = str_c(file_name, "_", particle_num), .keep = "unused", .before = 1
+)
+
+
+data |> write_rds("data/data.rds")
+
